141fab047c1445099cc34a1af92c0f376c1cccf2
Fabio Mangione, Konstantin Semenov and Tyson Gern <labs-pair+fmangione+kseme+tgern@pivotal.io>
2016-01-15 09:35:02 +0000

    Refactor queryParams construction.

--- frontend/app/components/meetups/meetup-service.ts
+++ frontend/app/components/meetups/meetup-service.ts
@@ -1,17 +1,21 @@
 import {Injectable} from 'angular2/core';
-import {Http} from 'angular2/http';
+import {Http, URLSearchParams} from 'angular2/http';
 import {Meetup} from './meetup-module';
 import {Observable} from 'rxjs/Observable';
 
-
 @Injectable()
 export class MeetupService implements Meetup.Provider {
-    constructor(public http:Http) {}
+  constructor(public http:Http) {
+  }
 
-    getList(city:string, countryCode:string):Observable<any> {
-        return this.http.get(`http://localhost:8080/meetups?city=${city}&countryCode=${countryCode}`)
-            .map(data => {
-                return JSON.parse(data.text()).map((o) => Meetup.Model.fromObject(o));
-            });
-    };
+  getList(city:string, countryCode:string):Observable<any> {
+    let queryParams = new URLSearchParams();
+    queryParams.set('city', city);
+    queryParams.set('countryCode', countryCode) ;
+
+    return this.http.get('http://localhost:8080/meetups', {search: queryParams})
+      .map(data => {
+        return JSON.parse(data.text()).map((o) => Meetup.Model.fromObject(o));
+      });
+  };
 }
